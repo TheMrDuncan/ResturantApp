cb2e95bff3a46c257722569cd4f0a1ae8bdb1563
Eddie Duncan <carl.e.duncan@icloud.com>
2022-10-22 17:14:11 -0500

    Initial Commit

---
+++ ResturantApp/ResturantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResturantAppApp.swift
+//  ResturantApp
+//
+//  Created by Carl Duncan on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ResturantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
